4c14f2a7c2ec2489a94b9819f5551a3aedcd4b4c
T-M24 <tm20220260@student.fon.bg.ac.rs>
2026-03-08 17:29:38 +0100

    Napravljene CRUD operacije i Controller na serverskoj strani

--- FrizerskiSalon_Server/src/repository/Repository.java
+++ FrizerskiSalon_Server/src/repository/Repository.java
@@ -11,7 +11,7 @@ import java.util.List;
  * @author Nikola Manjencic
  */
 public interface Repository<T> {
-    List<T> getAll(T param) throws Exception;
+    List<T> getAll(T param, String uslov) throws Exception;
     void add(T param) throws Exception;
     void edit(T param) throws Exception;
     void delete(T param) throws Exception;
---
+++ FrizerskiSalon_Server/src/repository_db_impl/DbRepositoryGeneric.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package repository_db_impl;
+
+import domen.AbstractDomainObject;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import repository_db.DBRepository;
+import repository_db.DbConnectionFactory;
+
+/**
+ *
+ * @author Nikola Manjencic
+ */
+public class DbRepositoryGeneric implements DBRepository<AbstractDomainObject> {
+
+    @Override
+    public List<AbstractDomainObject> getAll(AbstractDomainObject param, String uslov) throws Exception {
+        List<AbstractDomainObject> list = new ArrayList<>();
+        String upit = "SELECT * FROM " + param.getTableName();
+        if (uslov != null) { //ovde se vracam posle
+            upit += uslov;
+        }
+        System.out.println(upit);
+        Statement st = DbConnectionFactory.getInstance().getConnection().createStatement();
+        ResultSet rs = st.executeQuery(upit);
+        list = param.getList(rs);
+        rs.close();
+        st.close();
+        return list;
+    }
+
+    @Override
+    public void add(AbstractDomainObject param) throws Exception {
+        String upit = "INSERT INTO " + param.getTableName() + " (" + param.getInsertColumns()
+                + ") VALUES ( " + param.getInsertValues() + " )";
+        System.out.println(upit);
+        Statement st = DbConnectionFactory.getInstance().getConnection().createStatement();
+        st.executeUpdate(upit);
+        st.close();
+    }
+
+    @Override
+    public void edit(AbstractDomainObject param) throws Exception {
+        String upit = "UPDATE " + param.getTableName() + " SET " + param.getEditableValues();
+        System.out.println(upit);
+        Statement st = DbConnectionFactory.getInstance().getConnection().createStatement();
+        st.executeUpdate(upit);
+        st.close();
+
+    }
+
+    @Override
+    public void delete(AbstractDomainObject param) throws Exception {
+        String upit = "DELETE FROM " + param.getTableName() + " WHERE " + param.getPrimaryKey();
+        Statement st = DbConnectionFactory.getInstance().getConnection().createStatement();
+        st.executeUpdate(upit);
+        st.close();
+    }
+
+    @Override
+    public List<AbstractDomainObject> getAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
